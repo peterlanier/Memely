78ca8e88386b1714f7f7b52f25ea707bbd64962d
Peter Lanier <peterlanier@funyl.com>
2019-12-13 13:14:44 -0500

    sam init --runtime nodejs8.10

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import aws_exports from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react';
-import { Connect } from 'aws-amplify-react';
-import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import { Grid, Header, Input, List, Segment } from 'semantic-ui-react';
+import React, { Component } from "react";
+import aws_exports from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
+import { Connect } from "aws-amplify-react";
+import Amplify, { API, graphqlOperation, Storage } from 'aws-amplify';
+import {v4 as uuid} from 'uuid';
+import { Form, Grid, Header, Input, List, Segment } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
 
 Amplify.configure(aws_exports);
 
-
-function makeComparator(key, order = 'asc') {
+function makeComparator(key, order = "asc") {
   return (a, b) => {
     if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
 
-    const aVal = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
-    const bVal = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+    const aVal = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+    const bVal = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
 
     let comparison = 0;
     if (aVal > bVal) comparison = 1;
     if (aVal < bVal) comparison = -1;
 
-    return order === 'desc' ? (comparison * -1) : comparison
+    return order === "desc" ? comparison * -1 : comparison;
   };
 }
-
 
 const ListAlbums = `query ListAlbums {
   listAlbums(limit: 9999) {
@@ -42,18 +43,58 @@ const SubscribeToNewAlbums = `
   }
 `;
 
+const GetAlbum = `query GetAlbum($id: ID!) {
+  getAlbum(id: $id) {
+    id
+    name
+  }
+}
+`;
+
+class AlbumDetails extends Component {
+  render() {
+    return (
+      <Segment>
+        <Header as='h3'>{this.props.album.name}</Header>
+        <S3ImageUpload albumId={this.props.album.id}/>        
+        <p>TODO: Show photos for this album</p>
+      </Segment>
+    )
+  }
+}
+
+class AlbumDetailsLoader extends React.Component {
+  render() {
+    return (
+      <Connect query={graphqlOperation(GetAlbum, { id: this.props.id })}>
+        {({ data, loading, errors }) => {
+          if (loading) {
+            return <div>Loading...</div>;
+          }
+          if (errors.length > 0) {
+            return <div>{JSON.stringify(errors)}</div>;
+          }
+          if (!data.getAlbum) return;
+          return <AlbumDetails album={data.getAlbum} />;
+        }}
+      </Connect>
+    );
+  }
+}
 
 class AlbumsList extends React.Component {
   albumItems() {
-    return this.props.albums.sort(makeComparator('name')).map(album =>
-      <List.Item key={album.id}>{album.name}</List.Item>
-    );
+    return this.props.albums.sort(makeComparator("name")).map(album => (
+      <List.Item key={album.id}>
+        <NavLink to={`/albums/${album.id}`}>{album.name}</NavLink>
+      </List.Item>
+    ));
   }
 
   render() {
     return (
       <Segment>
-        <Header as='h3'>My Albums</Header>
+        <Header as="h3">My Albums</Header>
         <List divided relaxed>
           {this.albumItems()}
         </List>
@@ -62,13 +103,14 @@ class AlbumsList extends React.Component {
   }
 }
 
-
 class AlbumsListLoader extends React.Component {
   onNewAlbum = (prevQuery, newData) => {
     let updatedQuery = Object.assign({}, prevQuery);
-    updatedQuery.listAlbums.items = prevQuery.listAlbums.items.concat([newData.onCreateAlbum]);
+    updatedQuery.listAlbums.items = prevQuery.listAlbums.items.concat([
+      newData.onCreateAlbum
+    ]);
     return updatedQuery;
-  }
+  };
 
   render() {
     return (
@@ -78,7 +120,9 @@ class AlbumsListLoader extends React.Component {
         onSubscriptionMsg={this.onNewAlbum}
       >
         {({ data, loading, errors }) => {
-          if (loading) { return <div>Loading...</div>; }
+          if (loading) {
+            return <div>Loading...</div>;
+          }
           if (!data.listAlbums) return;
 
           return <AlbumsList albums={data.listAlbums.items} />;
@@ -88,18 +132,17 @@ class AlbumsListLoader extends React.Component {
   }
 }
 
-
 class NewAlbum extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      albumName: ''
+      albumName: ""
     };
   }
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
 
-  handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
     const NewAlbum = `mutation NewAlbum($name: String!) {
           createAlbum(input: {name: $name}) {
@@ -108,47 +151,103 @@ class NewAlbum extends Component {
           }
       }`;
     try {
-      const result = await API.graphql(graphqlOperation(NewAlbum, { name: this.state.albumName }));
+      const result = await API.graphql(
+        graphqlOperation(NewAlbum, { name: this.state.albumName })
+      );
       console.info(`Created album with id ${result.data.createAlbum.id}`);
-      this.setState({ albumName: ''});
+      this.setState({ albumName: "" });
+    } catch (err) {
+      console.error("NewAlbum mutation failed", err);
     }
-    catch (err) {
-      console.error('NewAlbum mutation failed', err);
-    }
-  }
+  };
 
   render() {
     return (
       <Segment>
-        <Header as='h3'>Add a new album</Header>
+        <Header as="h3">Add a new album</Header>
         <Input
-          type='text'
-          placeholder='New Album Name'
-          icon='plus'
-          iconPosition='left'
-          action={{ content: 'Create', onClick: this.handleSubmit }}
-          name='albumName'
+          type="text"
+          placeholder="New Album Name"
+          icon="plus"
+          iconPosition="left"
+          action={{ content: "Create", onClick: this.handleSubmit }}
+          name="albumName"
           value={this.state.albumName}
           onChange={this.handleChange}
         />
       </Segment>
-    )
-  }
-}
-
-
-class App extends Component {
-  render() {
-    return (
-      <Grid padded>
-        <Grid.Column>
-          <NewAlbum />
-          <AlbumsListLoader />
-        </Grid.Column>
-      </Grid>
     );
   }
 }
 
+class S3ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { uploading: false }
+  }
+  onChange = async (e) => {
+    const file = e.target.files[0];
+    const fileName = uuid();
+    this.setState({uploading: true});
+    const result = await Storage.put(
+      fileName, 
+      file, 
+      {
+        customPrefix: { public: 'uploads/' },
+        metadata: { albumid: this.props.albumId }
+      }
+    );
+    console.log('Uploaded file: ', result);
+    this.setState({uploading: false});
+  }
+  render() {
+    return (
+      <div>
+        <Form.Button
+          onClick={() => document.getElementById('add-image-file-input').click()}
+          disabled={this.state.uploading}
+          icon='file image outline'
+          content={ this.state.uploading ? 'Uploading...' : 'Add Image' }
+        />
+        <input
+          id='add-image-file-input'
+          type="file"
+          accept='image/*'
+          onChange={this.onChange}
+          style={{ display: 'none' }}
+        />
+      </div>
+    );
+  }
+}
 
-export default withAuthenticator(App, { includeGreetings: true }); 
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Grid padded>
+          <Grid.Column>
+            <Route path="/" exact component={NewAlbum} />
+            <Route path="/" exact component={AlbumsListLoader} />
+            <Route
+              path="/albums/:albumId"
+              render={() => (
+                <div>
+                  <NavLink to="/">Back to Albums list</NavLink>
+                </div>
+              )}
+            />
+            <Route
+              path="/albums/:albumId"
+              render={props => (
+                <AlbumDetailsLoader id={props.match.params.albumId} />
+              )}
+            />
+          </Grid.Column>
+        </Grid>
+      </Router>
+    );
+  }
+}
+
+export default withAuthenticator(App, { includeGreetings: true });
